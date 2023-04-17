732c9370115fc5aa144ae8c5999d2b895c4d8c71
Charles Costa <chlima88@gmail.com>
2023-04-17 02:24:19 -0300

    first steps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Sidebar from '$lib/Sidebar.svelte';
+	import Controls from '$lib/Controls.svelte';
+	import Main from '$lib/Main.svelte';
+	import '../app.css';
+</script>
+
+<div class="flex flex-col w-full h-screen">
+	<div class="flex flex-1">
+		<Sidebar />
+		<Main />
+	</div>
+	<Controls />
+</div>
