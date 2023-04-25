f44d3f25a02edb920dd0a158bb31263dcfe09cba
Charles Costa <chlima88@gmail.com>
2023-04-25 20:38:18 -0300

    add duration to the music

--- src/lib/data/songs.js
+++ src/lib/data/songs.js
@@ -3,96 +3,112 @@ export const audioLibrary = [
 		artist: 'Artist 1',
 		name: 'Ambient Piano and Strings',
 		url: 'https://cj-lab.s3.amazonaws.com/please-calm-my-mind-125566.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png'
+		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png',
+		duration: '2:55'
 	},
 	{
 		artist: 'Artist 2',
 		name: 'Please Calm My Mind',
 		url: 'https://cj-lab.s3.amazonaws.com/ambient-piano-ampamp-strings-10711.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png'
+		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png',
+		duration: '3:37'
 	},
 	{
 		artist: 'Artist 3',
 		name: 'Easy lifestyle',
 		url: 'https://cj-lab.s3.amazonaws.com/easy-lifestyle-137766.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png'
+		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png',
+		duration: '2:45'
 	},
 	{
 		artist: 'Artist 4',
 		name: 'Guitar Electro Sport Trailer',
 		url: 'https://cj-lab.s3.amazonaws.com/guitar-electro-sport-trailer-115571.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png'
+		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png',
+		duration: '1:58'
 	},
 	{
 		artist: 'Jason Ross',
 		name: 'Into You (Feat. Kara)',
 		url: 'https://cj-lab.s3.amazonaws.com/JasonRoss-IntoYou.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/JasonRoss.png'
+		image: 'https://cj-lab.s3.amazonaws.com/JasonRoss.png',
+		duration: '4:04'
 	},
 	{
 		artist: 'Skrux',
 		name: 'Escapade',
 		url: 'https://cj-lab.s3.amazonaws.com/Skrux-Escapade.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/Skrux.png'
+		image: 'https://cj-lab.s3.amazonaws.com/Skrux.png',
+		duration: '4:58'
 	},
 	{
 		artist: 'Trivecta',
 		name: 'Now You Know',
 		url: 'https://cj-lab.s3.amazonaws.com/Trivecta-NowYoukNow.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/trivecta.png'
+		image: 'https://cj-lab.s3.amazonaws.com/trivecta.png',
+		duration: '3:44'
 	},
 	{
 		artist: 'Virtual Riot',
 		name: 'Lunar',
 		url: 'https://cj-lab.s3.amazonaws.com/VirtualRiot-Lunar.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/VirtualRiot.jpg'
+		image: 'https://cj-lab.s3.amazonaws.com/VirtualRiot.jpg',
+		duration: '4:26'
 	},
 	{
 		artist: 'Artist 1',
 		name: 'Ambient Piano and Strings',
 		url: 'https://cj-lab.s3.amazonaws.com/please-calm-my-mind-125566.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png'
+		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png',
+		duration: '2:55'
 	},
 	{
 		artist: 'Artist 2',
 		name: 'Please Calm My Mind',
 		url: 'https://cj-lab.s3.amazonaws.com/ambient-piano-ampamp-strings-10711.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png'
+		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png',
+		duration: '3:37'
 	},
 	{
 		artist: 'Artist 3',
 		name: 'Easy lifestyle',
 		url: 'https://cj-lab.s3.amazonaws.com/easy-lifestyle-137766.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png'
+		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png',
+		duration: '2:45'
 	},
 	{
 		artist: 'Artist 4',
 		name: 'Guitar Electro Sport Trailer',
 		url: 'https://cj-lab.s3.amazonaws.com/guitar-electro-sport-trailer-115571.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png'
+		image: 'https://cj-lab.s3.amazonaws.com/spoticlonelogo.png',
+		duration: '1:58'
 	},
 	{
 		artist: 'Jason Ross',
 		name: 'Into You (Feat. Kara)',
 		url: 'https://cj-lab.s3.amazonaws.com/JasonRoss-IntoYou.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/JasonRoss.png'
+		image: 'https://cj-lab.s3.amazonaws.com/JasonRoss.png',
+		duration: '4:04'
 	},
 	{
 		artist: 'Skrux',
 		name: 'Escapade',
 		url: 'https://cj-lab.s3.amazonaws.com/Skrux-Escapade.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/Skrux.png'
+		image: 'https://cj-lab.s3.amazonaws.com/Skrux.png',
+		duration: '4:58'
 	},
 	{
 		artist: 'Trivecta',
 		name: 'Now You Know',
 		url: 'https://cj-lab.s3.amazonaws.com/Trivecta-NowYoukNow.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/trivecta.png'
+		image: 'https://cj-lab.s3.amazonaws.com/trivecta.png',
+		duration: '3:44'
 	},
 	{
 		artist: 'Virtual Riot',
 		name: 'Lunar',
 		url: 'https://cj-lab.s3.amazonaws.com/VirtualRiot-Lunar.mp3',
-		image: 'https://cj-lab.s3.amazonaws.com/VirtualRiot.jpg'
+		image: 'https://cj-lab.s3.amazonaws.com/VirtualRiot.jpg',
+		duration: '4:26'
 	}
 ];
